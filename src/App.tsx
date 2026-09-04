41e12e0e48fb81ad5f41f27c684c485d1d364a71
Hridoy-Ahmed-Rafi20 <hridoyahmedrafi20@gmail.com>
2026-09-04 17:29:10 +0600

    deleted space

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { Suspense } from "react";
 import "./App.css";
 import type { CountryType } from "./type";
 import Countries from "./components/Countries/Countries";
-
 // create a promise to loard data
 const countriesPromise = async (): Promise<CountryType[]> => {
   const res = await fetch("https://openapi.programming-hero.com/api/all");
@@ -14,7 +13,6 @@ function App() {
   return (
     <>
       <h1>Explore the world</h1>
-
       <Suspense fallback="Loading...">
         <Countries countriesPromise={countriesPromise()}></Countries>
       </Suspense>
